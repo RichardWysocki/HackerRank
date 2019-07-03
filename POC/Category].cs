35ddbdce74156223f65420d7282900745e1ced40
Richad Wysocki <RichardWysocki@gmail.com>
2019-07-03 13:06:10 -0400

    Add Changes for Pull

---
+++ POC/Category].cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POC
+{
+    public class Category
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+    }
+}
---
+++ POC/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POC
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int CategoryID { get; set; }
+    }
+}
