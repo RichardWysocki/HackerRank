eb8e6be712711a204b0e0603e08b748b7d17eb5c
Richad Wysocki <RichardWysocki@gmail.com>
2019-10-04 15:23:48 -0400

    Add Interview Questions

---
+++ InterviewHackerrank/Interviews/Car_Refactor/CarDataModel.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewHackerrank.Interviews.Car_Refactor
+{
+
+    public class Media
+    {
+        public List<string> photo_links { get; set; }
+    }
+
+    public class Dealer
+    {
+        public int id { get; set; }
+        public string website { get; set; }
+        public string name { get; set; }
+        public string dealer_type { get; set; }
+        public string street { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string country { get; set; }
+        public string latitude { get; set; }
+        public string longitude { get; set; }
+        public string zip { get; set; }
+        public string phone { get; set; }
+    }
+
+    public class Build
+    {
+        public int year { get; set; }
+        public string make { get; set; }
+        public string model { get; set; }
+        public string trim { get; set; }
+        public string body_type { get; set; }
+        public string body_subtype { get; set; }
+        public string vehicle_type { get; set; }
+        public string transmission { get; set; }
+        public string drivetrain { get; set; }
+        public string fuel_type { get; set; }
+        public string engine { get; set; }
+        public double engine_size { get; set; }
+        public string engine_block { get; set; }
+        public int doors { get; set; }
+        public int cylinders { get; set; }
+        public string made_in { get; set; }
+        public string steering_type { get; set; }
+        public string antibrake_sys { get; set; }
+        public string tank_size { get; set; }
+        public string overall_height { get; set; }
+        public string overall_length { get; set; }
+        public string overall_width { get; set; }
+        public string std_seating { get; set; }
+        public string highway_miles { get; set; }
+        public string city_miles { get; set; }
+        public string trim_r { get; set; }
+    }
+
+    public class Listing
+    {
+        public string id { get; set; }
+        public string vin { get; set; }
+        public string heading { get; set; }
+        public int price { get; set; }
+        public int miles { get; set; }
+        public string data_source { get; set; }
+        public string vdp_url { get; set; }
+        public bool carfax_1_owner { get; set; }
+        public bool carfax_clean_title { get; set; }
+        public string exterior_color { get; set; }
+        public string interior_color { get; set; }
+        public int dom { get; set; }
+        public int dom_180 { get; set; }
+        public int dom_active { get; set; }
+        public string seller_type { get; set; }
+        public string inventory_type { get; set; }
+        public int last_seen_at { get; set; }
+        public DateTime last_seen_at_date { get; set; }
+        public int scraped_at { get; set; }
+        public DateTime scraped_at_date { get; set; }
+        public int first_seen_at { get; set; }
+        public DateTime first_seen_at_date { get; set; }
+        public int ref_price { get; set; }
+        public int ref_price_dt { get; set; }
+        public int ref_miles { get; set; }
+        public int ref_miles_dt { get; set; }
+        public string source { get; set; }
+        public Media media { get; set; }
+        public Dealer dealer { get; set; }
+        public Build build { get; set; }
+        public string stock_no { get; set; }
+        public int? msrp { get; set; }
+    }
+
+    public class RootObject
+    {
+        public int num_found { get; set; }
+        public List<Listing> listings { get; set; }
+    }
+}
---
+++ InterviewHackerrank/Interviews/Car_Refactor/Cars.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using InterviewHackerrank.Interviews.Car_Refactor;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace InterviewHackerrank.Car_Refactor
+{
+
+    class Rich_TestMovieTitles
+    {
+
+        [Test]
+        public void TestMovies()
+        {
+            //Arrange
+            var movies =
+                new Cars("https://marketcheck-prod.apigee.net/v1/search?api_key=GPSyUoNIdLcxhQt1MPGA8ALNC2EVY9yX&year=2019&start=0&rows=50");
+
+            //Act
+            var response = movies.getCars("Ford");
+
+            //Assert
+            Assert.That(response.Length == 13, response.Count().ToString);
+
+
+        }
+
+        public class Cars
+        {
+            private readonly string _url;
+                
+
+            public Cars(string url)
+            {
+                _url = url;
+                LoadJson();
+
+
+            }
+
+            public void LoadJson()
+            {
+                Console.WriteLine(Directory.GetCurrentDirectory());
+
+                var dir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                using (StreamReader r = new StreamReader(dir+ @"\Car_Refactor\Cars.json"))
+                {
+                    string json = r.ReadToEnd();
+                    var items = JsonConvert.DeserializeObject<RootObject>(json);
+                }
+
+            }
+
+            public string[] getCars(string seachTitle)
+            {
+                var titles = new List<string>();
+                var httpData = new HttpClient();
+                var knt = 1;
+                var jsonTask = CarData(seachTitle, httpData, titles, knt);
+                //var kntpages = jsonTask.total_pages;
+                //while (knt < kntpages)
+                //{
+                //    knt++;
+                //    MovieData(seachTitle, httpData, titles, knt);
+                //}
+                return titles.OrderBy(x => x).ToArray();
+
+            }
+
+            private RootObject CarData(string seachTitle, HttpClient httpData, List<string> titles, int pageNumber)
+            {
+                var response = httpData.GetAsync(_url).Result;
+                var jsonString = response.Content.ReadAsStringAsync().Result;
+                var jsonTask = JsonConvert.DeserializeObject<RootObject>(jsonString);
+                foreach (var movelist in jsonTask.listings)
+                {
+                    titles.Add(movelist.build.model);
+                }
+
+                return jsonTask;
+            }
+
+
+        }
+
+        
+
+
+    }
+}
