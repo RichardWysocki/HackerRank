2d4dab2b37d46b8da4c8b8faa683954b603a2cdc
Richad Wysocki <RichardWysocki@gmail.com>
2019-10-18 15:10:41 -0400

    Update Counter in Blazor app -

---
+++ BlazerApp_PhillydotNet_Test/UnitTest1.cs
@@ -0,0 +1,29 @@
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using NUnit.Framework;
+
+
+namespace BlazerApp_PhillydotNet_Test
+{
+    public class Tests
+    {
+       //private TestHost host = new TestHost();
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            //Arrange
+            //var componentHost = new TestHost();
+
+            ////Act
+            //var com[ = componentHost<]
+
+            //Assert
+            Assert.Pass();
+        }
+    }
+}
